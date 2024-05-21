797b5d5e4e7335b87cb85e5068c792711afeacda
cutanobi <167220489+cutanobi@users.noreply.github.com>
2024-05-21 15:05:17 +0530

    Update read.py

--- Asuraa/modules/helpers/read.py
+++ Asuraa/modules/helpers/read.py
@@ -11,7 +11,7 @@ TOOLS_DATA_READ = f"""
 ๏ **ʜᴇʀᴇ ɪs ᴛᴏᴏʟs ғᴏʀ {BOT_NAME} ! \n\n๏ ᴜsᴇ /repo ɢᴇᴛᴛɪɴɢ ᴍʏ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ. \n\n๏ ᴜsᴇ /ping ᴄʜᴇᴄᴋɪɴɢ ғᴏʀ ᴘɪɴɢ.\n\n๏ ᴜsᴇ /id ɢᴇᴛᴛɪɴɢ ʏᴏᴜʀ ᴄʜᴀᴛ ɪᴅ ᴀɴᴅ ᴜsᴇʀ ɪᴅ ᴏʀ ᴍᴇssᴀɢᴇ ɪᴅ."""
 
 CHATBOT_READ = f"""
-๏** ᴄᴏᴍᴍᴀɴᴅ ғᴏʀ {BOT_NAME} !\n\n๏ ᴜsᴇ /chatbot ᴇɴᴀʙʟᴇ/ᴅɪsᴀʙʟᴇ .\n\n๏ ᴍᴀᴅᴇ ʙʏ ➛ [ɢᴏᴅ ʀᴀᴠᴀɴ](https://t.me/God_Ravana)**"""
+๏** ᴄᴏᴍᴍᴀɴᴅ ғᴏʀ {BOT_NAME} !\n\n๏ ᴜsᴇ /chatbot ᴇɴᴀʙʟᴇ/ᴅɪsᴀʙʟᴇ .\n\n๏ ᴍᴀᴅᴇ ʙʏ ➛ [BAIRAGI](https://t.me/ll_BAIRAGI_ll)**"""
 
 SOURCE_READ = f"**❖ ʜᴇʏ ʙᴀʙʏ, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ ♥︎
 
